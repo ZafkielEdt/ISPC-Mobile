46439d61b815d67448427778cc93bb2762811b69
Pautk <pautk7.6@gmail.com>
2023-09-27 17:19:49 -0300

    Activity E-commerce creada.

---
+++ app/src/main/java/com/ispc/gymapp/Activity_Ecommerce.java
@@ -0,0 +1,14 @@
+package com.ispc.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Ecommerce extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecommerce);
+    }
+}
