9a5b612a0484f06b2972ef9730cddf393f036b81
Franco Lorca <lorcafrancog@proton.me>
2023-10-06 12:03:50 -0300

    Update(exercisesIntermediate.java,.xml): Add recyclerView

--- app/src/main/java/com/ispc/gymapp/views/fragments/ExercisesIntermediateFragment.java
+++ app/src/main/java/com/ispc/gymapp/views/fragments/ExercisesIntermediateFragment.java
@@ -3,12 +3,22 @@ package com.ispc.gymapp.views.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ispc.gymapp.R;
+import com.ispc.gymapp.model.Exercise;
+import com.ispc.gymapp.views.adapter.ExerciseListAdapter;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +26,14 @@ import com.ispc.gymapp.R;
  * create an instance of this fragment.
  */
 public class ExercisesIntermediateFragment extends Fragment {
+
+    RecyclerView recyclerView;
+
+    ArrayList<Exercise> exercises;
+
+    ExerciseListAdapter exerciseListAdapter;
+
+    FirebaseFirestore db;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +79,40 @@ public class ExercisesIntermediateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercises_intermediate, container, false);
+        View view = inflater.inflate(R.layout.fragment_exercises_intermediate, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerIntermediate);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        db = FirebaseFirestore.getInstance();
+        exercises = new ArrayList<>();
+        String exerciseType = "beginner";
+        exerciseListAdapter = new ExerciseListAdapter(view.getContext(), exercises);
+
+        recyclerView.setAdapter(exerciseListAdapter);
+
+        getIntermediateExercises();
+
+        return view;
+    }
+
+    private void getIntermediateExercises() {
+        db.collection("exercises").orderBy("title").whereEqualTo("type", "intermediate")
+                .addSnapshotListener((value, error) -> {
+                    if (error != null) {
+                        Log.e("Firestore error", Objects.requireNonNull(error.getMessage()));
+                        return;
+                    }
+
+                    for (DocumentChange documentChange : value.getDocumentChanges()) {
+
+                        if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                            exercises.add(documentChange.getDocument().toObject(Exercise.class));
+                        }
+
+                        exerciseListAdapter.notifyDataSetChanged();
+                    }
+                });
     }
 }
