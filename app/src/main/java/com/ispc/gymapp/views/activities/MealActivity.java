6a895405bbd5ae17050275d74b5a0ba2d55f42fa
federico42o <fedes7777@gmail.com>
2023-10-10 21:37:40 -0300

    add meal activity

---
+++ app/src/main/java/com/ispc/gymapp/views/activities/MealActivity.java
@@ -0,0 +1,16 @@
+package com.ispc.gymapp.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ispc.gymapp.R;
+
+public class MealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal);
+    }
+}
