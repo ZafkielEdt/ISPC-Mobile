91c0fc82e69077e501e276cf5bb74658fb5331bb
Franco Lorca <lorcafrancog@proton.me>
2023-10-18 15:15:13 -0300

    Update(RoutineActivity.java): Implement new changes of RoutineAdapter

--- app/src/main/java/com/ispc/gymapp/views/activities/RoutineActivity.java
+++ app/src/main/java/com/ispc/gymapp/views/activities/RoutineActivity.java
@@ -6,57 +6,60 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.ispc.gymapp.R;
+import com.ispc.gymapp.model.Exercise;
 import com.ispc.gymapp.model.Routine;
+import com.ispc.gymapp.model.User;
 import com.ispc.gymapp.views.adapter.RoutineAdapter;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class RoutineActivity extends AppCompatActivity {
 
     RoutineAdapter routineAdapter;
 
-    ArrayList<Routine> routines;
+    ArrayList<Exercise> exercises;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine);
-
         setUpRecyclerView();
     }
 
     private void getRoutines() {
-        db.collection("routines").orderBy("title").addSnapshotListener((value, error) -> {
-            if (error != null) {
-                Log.e("Firestore error", Objects.requireNonNull(error.getMessage()));
-                return;
-            }
+        db.collection("routines").whereEqualTo("user", user.getMail())
+                .get().addOnCompleteListener(task -> {
 
-            for (DocumentChange documentChange : value.getDocumentChanges()) {
-
-                if (documentChange.getType() == DocumentChange.Type.ADDED) {
-                    routines.add(documentChange.getDocument().toObject(Routine.class));
-                }
-
-                routineAdapter.notifyDataSetChanged();
-            }
-        });
+                    for (DocumentChange documentChange : task.getResult().getDocumentChanges()) {
+                        Routine currentRoutine = new Routine();
+                        if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                            currentRoutine = documentChange.getDocument().toObject(Routine.class);
+                            exercises.addAll(currentRoutine.getExercises());
+                            routineAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
     }
 
     private void setUpRecyclerView() {
-        routines = new ArrayList<>();
+        exercises = new ArrayList<>();
         // Adapter
-        routineAdapter = new RoutineAdapter(this, routines);
-        getRoutines();
+        routineAdapter = new RoutineAdapter(this, exercises);
+        getUser();
         // Recycler
         RecyclerView recyclerView = findViewById(R.id.recyclerRoutine);
         recyclerView.setAdapter(routineAdapter);
@@ -70,5 +73,20 @@ public class RoutineActivity extends AppCompatActivity {
     public void returnToExerciseList(View view) {
         Intent intent = new Intent(this, ExerciseList.class);
         startActivity(intent);
+    }
+
+    private void getUser() {
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            DocumentReference usernameRef = db.collection("users").document(firebaseUser.getUid());
+            usernameRef.get().addOnSuccessListener(documentSnapshot -> {
+                if (documentSnapshot.exists()) {
+
+                    user = documentSnapshot.toObject(User.class);
+                    getRoutines();
+                }
+            });
+
+        }
     }
 }
